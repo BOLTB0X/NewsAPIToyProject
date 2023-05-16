40518271f0c651eeaba1263009a56ec73b57440e
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-05-16 14:21:31 +0900

    Model+API 재정의
    몇몇 프로퍼티 옵셔널로 재정의

--- newsAPIToyApp/newsAPIToyApp/Model+API.swift
+++ newsAPIToyApp/newsAPIToyApp/Model+API.swift
@@ -11,29 +11,21 @@ import Combine
 
 // MARK: - API요청으로 인한 응답: articles
 struct APIResults: Codable {
-//    let status: String?
-//    let totalResults: Int?
+    let status: String
+    let totalResults: Int
     let articles: [Article]
-//
-//    enum CodingKeys: String, CodingKey {
-//        case status, totalResults, articles
-//    }
-
 }
 
 // MARK: - Article
 struct Article: Codable, Hashable {
-//    let author: String?
+    //let source: Source
+    let author: String?
     let title: String
-//    let description: String?
-//    let url: String?
-//    let urlToImage: String?
-//    let publishedAt: Date?
-//    let content: String?
-    
-//    enum CodingKeys: String, CodingKey {
-//        case author, title, description, url, urlToImage, publishedAt, content
-//    }
+    let description: String?
+    let url: String
+    let urlToImage: String?
+    let publishedAt: String
+    let content: String?
 
 }
 
