2c56dcfdc3f1b6793d7dc6c06f66df54ce79ba6f
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-06-27 18:32:55 +0900

    검색 및 뉴스메인 수정

--- newsAPIToy02/01newsAPIToyApp /newsAPIToyApp/Views/Everything/Everything.swift
+++ newsAPIToy02/01newsAPIToyApp /newsAPIToyApp/Views/Everything/Everything.swift
@@ -12,26 +12,32 @@ struct Everything: View {
     @State private var inputText: String = ""
     
     var body: some View {
-        NavigationView {
-            VStack {
-                SearchBar(text: $inputText, startSearch: {
-                    Task {
-                        do {
-                            try await everyViewModel.fetchNewsEverythingOnServer(query: inputText)
-                        } catch {
-                            // 오류 처리
-                            print("Error: \(error)")
-                        }
+        VStack {
+            SearchBar(text: $inputText, startSearch: {
+                Task {
+                    do {
+                        try await everyViewModel.fetchNewsEverythingOnServer(query: inputText)
+                        
+                    } catch {
+                        // 오류 처리
+                        print("Error: \(error)")
                     }
-                })
-                
-                if !everyViewModel.items.isEmpty {
-                    List { // ForEach로 담겨진 뉴스기사 배열을 깔끔히 처리를 위해 List를 사용
-                        ForEach(everyViewModel.items) { result in
-                            CardView(title: result.title, cate: result.author!, imgURL: result.urlToImage!, date: result.publishedAt)
+                }
+            })
+            
+            if !everyViewModel.items.isEmpty {
+                List { // ForEach로 담겨진 뉴스기사 배열을 깔끔히 처리를 위해 List를 사용
+                    ForEach(everyViewModel.items) { result in
+                        NavigationLink(destination: WebView(urlToLoad: result.url)) {
+                            Text(result.title)
                         }
                     }
                 }
+                .listStyle(.grouped)
+            } else {
+                Spacer()
+                Text("Please!!")
+                Spacer()
             }
         }
     }
